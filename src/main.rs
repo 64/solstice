d36cda96667150da363fc5185556cf6a689330cd
Matt Taylor <mstaveleytaylor@gmail.com>
2019-06-21 22:04:59 +0100

    Add log crate backend implementation (provides access to info, error, warn, debug macros)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #![no_std]
 #![no_main]
 
-extern crate ddos_ds as ds;
+#[macro_use]
+extern crate log;
 
 #[macro_use]
 extern crate ddos_drivers as drivers;
+
+extern crate ddos_ds as ds;
 
 use core::panic::PanicInfo;
 
@@ -16,11 +19,13 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("HELLO WORLD");
-    println!("something that is so much longer than 80 characters aka the width of the console of this project oh, and a few\t1\t2\t3\t4\t5 tabs :)\n1\n2\n3 \\n's aswel :)");
-    let mut i = 0;
-    loop {
-        println!("test {}", i);
-        i += 1;
-    }
+    drivers::vga_text::init().unwrap();
+
+    debug!("test 1");
+    info!("test 2");
+    warn!("test 3");
+    error!("test 4");
+
+    #[allow(clippy::empty_loop)]
+    loop {}
 }
