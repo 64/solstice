a5790cf9e157c1925a605d42d292aef4af0f81a4
Yuhanun Citgez <49172424+Yuhanun@users.noreply.github.com>
2019-06-21 21:50:00 +0200

    Handled scrolling, \n, \t, and general cursor positions in VGA text mode (#2)
    * Escape characters, next lines, tabs and cursor positions are now handled.
    
    * Scrolling is now handled, tested, works fine.
    
    * Fixed all suggestions
    
    * Removed some deprecated code

--- ddos_drivers/src/vga_text.rs
+++ ddos_drivers/src/vga_text.rs
@@ -3,7 +3,7 @@ use ddos_ds::SpinLock;
 use lazy_static::lazy_static;
 
 const WIDTH: usize = 80;
-// const HEIGHT: usize = 25;
+const HEIGHT: usize = 25;
 
 pub struct Writer {
     buf: &'static mut [u16],
@@ -12,10 +12,73 @@ pub struct Writer {
 }
 
 impl Writer {
+    /// Handles escape characters for writing to the screen.
+
+    /// Done: \n, \t
+    /// TODO: All the other escape characters (\r, etc...)
+
+    /// Returns true if the character is an escape character, false if it's not.
+    /// If it's an escape character, then the `x` and `y` positions do not
+    /// have to be incremented because that's handled inside this function.
+    pub fn handle_escapes(&mut self, ch: u8) -> bool {
+        match ch {
+            b'\n' => {
+                self.y += 1;
+                self.x = 0;
+                return true;
+            }
+            b'\t' => {
+                self.write_string("    ");
+                return true;
+            }
+            _ => {
+                return false;
+            }
+        }
+    }
+
+    /// Handles scrolling
+    /// In essence this just pushes everything up by decrementing their `y` position by 1
+    /// This overwrites row 0
+    pub fn handle_scrolling(&mut self) {
+        if self.y < HEIGHT {
+            return;
+        }
+        unsafe {
+            let begin_ptr: *mut u16 = self.buf[..].as_mut_ptr();
+            core::intrinsics::volatile_copy_memory(
+                begin_ptr,
+                begin_ptr.offset(WIDTH as isize),
+                WIDTH * (HEIGHT - 1),
+            );
+        }
+        self.y = HEIGHT - 1;
+        self.x = 0;
+    }
+
+    /// Handles position of the cursor, does not handle scrolling.
+    pub fn update_cursor_position(&mut self) {
+        self.x += 1;
+        if self.x < WIDTH {
+            return;
+        }
+        self.x = 0;
+        self.y += 1;
+    }
+
+    /// Write a byte to the screen, handles escape characters and updates positions.
     pub fn write_byte(&mut self, ch: u8) {
+        // Handle escape characters.
+        if self.handle_escapes(ch) {
+            return;
+        }
+
+        self.handle_scrolling();
+
+        // Actually write the character to the screen, escapes have been handled previously no need to worry about those anymore.
         self.buf[self.y * WIDTH + self.x] = (0x0B << 8) | u16::from(ch);
 
-        self.x += 1; // TODO: Properly handle newlines, tabs, scrolling etc
+        self.update_cursor_position();
     }
 
     pub fn write_string(&mut self, s: &str) {
