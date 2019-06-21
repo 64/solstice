8a4fd60d81dea34a682775a48c028aa9975fd135
DrCrally <42429221+DrCrally@users.noreply.github.com>
2019-06-21 18:07:53 -0500

    Initial GDT (#3)
    * panic! prints diagnostic message
    
    * Remove ide files
    
    * gdt with kernel code segment

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,31 @@ extern crate ddos_drivers as drivers;
 extern crate ddos_ds as ds;
 
 use core::panic::PanicInfo;
+use x86_64::structures;
+use x86_64::structures::gdt::Descriptor;
+use x86_64::structures::gdt::GlobalDescriptorTable;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref GDT: GlobalDescriptorTable = {
+        let mut gdt = GlobalDescriptorTable::new();
+        gdt.add_entry(structures::gdt::Descriptor::UserSegment(0));
+        gdt.add_entry(structures::gdt::Descriptor::kernel_code_segment());
+        gdt
+    };
+}
 
 #[panic_handler]
 #[allow(clippy::empty_loop)]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    error!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("HELLO WORLD");
+    println!("something that is so much longer than 80 characters aka the width of the console of this project oh, and a few\t1\t2\t3\t4\t5 tabs :)\n1\n2\n3 \\n's aswel :)");
     drivers::vga_text::init().unwrap();
 
     debug!("test 1");
@@ -28,6 +44,8 @@ pub extern "C" fn _start() -> ! {
 
     let x = "test 5";
     dbg!(x);
+
+    GDT.load();
 
     #[allow(clippy::empty_loop)]
     loop {}
