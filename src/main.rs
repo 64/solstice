9a6a59ab82b96fea4a2a748099e52b875231cebc
Matt Taylor <mstaveleytaylor@gmail.com>
2019-06-22 00:36:10 +0100

    Move GDT loading into separate module, halt CPU in spin loop after main

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,9 @@ extern crate ddos_drivers as drivers;
 
 extern crate ddos_ds as ds;
 
+mod cpu;
+
 use core::panic::PanicInfo;
-use x86_64::structures;
-use x86_64::structures::gdt::Descriptor;
-use x86_64::structures::gdt::GlobalDescriptorTable;
-use lazy_static::lazy_static;
-
-lazy_static! {
-    static ref GDT: GlobalDescriptorTable = {
-        let mut gdt = GlobalDescriptorTable::new();
-        gdt.add_entry(structures::gdt::Descriptor::UserSegment(0));
-        gdt.add_entry(structures::gdt::Descriptor::kernel_code_segment());
-        gdt
-    };
-}
-
 #[panic_handler]
 #[allow(clippy::empty_loop)]
 fn panic(info: &PanicInfo) -> ! {
@@ -33,9 +21,9 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("HELLO WORLD");
-    println!("something that is so much longer than 80 characters aka the width of the console of this project oh, and a few\t1\t2\t3\t4\t5 tabs :)\n1\n2\n3 \\n's aswel :)");
     drivers::vga_text::init().unwrap();
+
+    cpu::gdt::load();
 
     debug!("test 1");
     info!("test 2");
@@ -45,8 +33,8 @@ pub extern "C" fn _start() -> ! {
     let x = "test 5";
     dbg!(x);
 
-    GDT.load();
-
-    #[allow(clippy::empty_loop)]
-    loop {}
+    loop {
+        // x86_64::instructions::interrupts::enable();
+        x86_64::instructions::hlt();
+    }
 }
