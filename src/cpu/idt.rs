8ceb13500aa967195172d53efe44d82e7069845b
Craly <fido8956@gmail.com>
2019-06-23 16:55:36 -0500

    Fix formatting in idt.rs and kernel.rs

--- src/cpu/idt.rs
+++ src/cpu/idt.rs
@@ -6,33 +6,24 @@ lazy_static! {
         let mut idt = idt::InterruptDescriptorTable::new();
         idt.divide_by_zero.set_handler_fn(divide_by_zero_handler);
         idt.debug.set_handler_fn(debug_handler);
-        idt.non_maskable_interrupt
-            .set_handler_fn(non_maskable_interrupt_handler);
+        idt.non_maskable_interrupt.set_handler_fn(non_maskable_interrupt_handler);
         idt.breakpoint.set_handler_fn(breakpoint_handler);
         idt.overflow.set_handler_fn(overflow_handler);
-        idt.bound_range_exceeded
-            .set_handler_fn(bound_range_exceeded_handler);
+        idt.bound_range_exceeded.set_handler_fn(bound_range_exceeded_handler);
         idt.invalid_opcode.set_handler_fn(invalid_opcode_handler);
-        idt.device_not_available
-            .set_handler_fn(device_not_available_handler);
+        idt.device_not_available.set_handler_fn(device_not_available_handler);
         idt.double_fault.set_handler_fn(double_fault_handler);
         idt.invalid_tss.set_handler_fn(invalid_tss_handler);
-        idt.segment_not_present
-            .set_handler_fn(segment_not_present_handler);
-        idt.stack_segment_fault
-            .set_handler_fn(stack_segment_fault_handler);
-        idt.general_protection_fault
-            .set_handler_fn(general_protection_fault_handler);
+        idt.segment_not_present.set_handler_fn(segment_not_present_handler);
+        idt.stack_segment_fault.set_handler_fn(stack_segment_fault_handler);
+        idt.general_protection_fault.set_handler_fn(general_protection_fault_handler);
         idt.page_fault.set_handler_fn(page_fault_handler);
-        idt.x87_floating_point
-            .set_handler_fn(x87_floating_point_handler);
+        idt.x87_floating_point.set_handler_fn(x87_floating_point_handler);
         idt.alignment_check.set_handler_fn(alignment_check_handler);
         idt.machine_check.set_handler_fn(machine_check_handler);
-        idt.simd_floating_point
-            .set_handler_fn(simd_floating_point_handler);
+        idt.simd_floating_point.set_handler_fn(simd_floating_point_handler);
         idt.virtualization.set_handler_fn(virtualization_handler);
-        idt.security_exception
-            .set_handler_fn(security_exception_handler);
+        idt.security_exception.set_handler_fn(security_exception_handler);
         idt
     };
 }
@@ -74,78 +65,36 @@ extern "x86-interrupt" fn device_not_available_handler(frame: &mut idt::Interrup
     panic!("EXCEPTION: Device Not Available\n{:#?}", frame);
 }
 
-extern "x86-interrupt" fn double_fault_handler(
-    frame: &mut idt::InterruptStackFrame,
-    error_code: u64,
-) {
-    panic!(
-        "EXCEPTION: Double Fault with error code {}\n{:#?}",
-        error_code, frame
-    );
+extern "x86-interrupt" fn double_fault_handler(frame: &mut idt::InterruptStackFrame, error_code: u64) {
+    panic!("EXCEPTION: Double Fault with error code {}\n{:#?}", error_code, frame);
 }
 
-extern "x86-interrupt" fn invalid_tss_handler(
-    frame: &mut idt::InterruptStackFrame,
-    error_code: u64,
-) {
-    panic!(
-        "EXCEPTION: Invalid TSS with error code {}\n{:#?}",
-        error_code, frame
-    );
+extern "x86-interrupt" fn invalid_tss_handler(frame: &mut idt::InterruptStackFrame, error_code: u64) {
+    panic!("EXCEPTION: Invalid TSS with error code {}\n{:#?}", error_code, frame);
 }
 
-extern "x86-interrupt" fn segment_not_present_handler(
-    frame: &mut idt::InterruptStackFrame,
-    error_code: u64,
-) {
-    panic!(
-        "EXCEPTION: Segment Not Present with error code {}\n{:#?}",
-        error_code, frame
-    );
+extern "x86-interrupt" fn segment_not_present_handler(frame: &mut idt::InterruptStackFrame, error_code: u64) {
+    panic!("EXCEPTION: Segment Not Present with error code {}\n{:#?}", error_code, frame);
 }
 
-extern "x86-interrupt" fn stack_segment_fault_handler(
-    frame: &mut idt::InterruptStackFrame,
-    error_code: u64,
-) {
-    panic!(
-        "EXCEPTION: Stack Segment Fault with error code {}\n{:#?}",
-        error_code, frame
-    );
+extern "x86-interrupt" fn stack_segment_fault_handler(frame: &mut idt::InterruptStackFrame, error_code: u64) {
+    panic!("EXCEPTION: Stack Segment Fault with error code {}\n{:#?}", error_code, frame);
 }
 
-extern "x86-interrupt" fn general_protection_fault_handler(
-    frame: &mut idt::InterruptStackFrame,
-    error_code: u64,
-) {
-    panic!(
-        "EXCEPTION: General Protection Fault with error code {}\n{:#?}",
-        error_code, frame
-    );
+extern "x86-interrupt" fn general_protection_fault_handler(frame: &mut idt::InterruptStackFrame, error_code: u64) {
+    panic!("EXCEPTION: General Protection Fault with error code {}\n{:#?}", error_code, frame);
 }
 
-extern "x86-interrupt" fn page_fault_handler(
-    frame: &mut idt::InterruptStackFrame,
-    error_code: idt::PageFaultErrorCode,
-) {
-    panic!(
-        "EXCEPTION: Page Fault with error code {:#?}\n{:#?}",
-        error_code, frame
-    );
+extern "x86-interrupt" fn page_fault_handler(frame: &mut idt::InterruptStackFrame, error_code: idt::PageFaultErrorCode) {
+    panic!("EXCEPTION: Page Fault with error code {:#?}\n{:#?}", error_code, frame);
 }
 
 extern "x86-interrupt" fn x87_floating_point_handler(frame: &mut idt::InterruptStackFrame) {
     panic!("EXCEPTION: x87 Floating Point\n{:#?}", frame);
 }
 
-extern "x86-interrupt" fn alignment_check_handler(
-    frame: &mut idt::InterruptStackFrame,
-    error_code: u64,
-) {
-    panic!(
-        "EXCEPTION: Alignment Check with error code {}\n{:#?}",
-        error_code, frame
-    );
+extern "x86-interrupt" fn alignment_check_handler(frame: &mut idt::InterruptStackFrame, error_code: u64) {
+    panic!("EXCEPTION: Alignment Check with error code {}\n{:#?}", error_code, frame);
 }
 
 extern "x86-interrupt" fn machine_check_handler(frame: &mut idt::InterruptStackFrame) {
@@ -160,12 +109,6 @@ extern "x86-interrupt" fn virtualization_handler(frame: &mut idt::InterruptStack
     panic!("EXCEPTION: Virtualization\n{:#?}", frame);
 }
 
-extern "x86-interrupt" fn security_exception_handler(
-    frame: &mut idt::InterruptStackFrame,
-    error_code: u64,
-) {
-    panic!(
-        "EXCEPTION: Security Exception with error code {}\n{:#?}",
-        error_code, frame
-    );
+extern "x86-interrupt" fn security_exception_handler(frame: &mut idt::InterruptStackFrame, error_code: u64, ) {
+    panic!("EXCEPTION: Security Exception with error code {}\n{:#?}", error_code, frame);
 }
